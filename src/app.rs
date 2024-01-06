28fcdd361c6b7f00ed98c1b3aa2d146049ed31be
ather <rizviather6@gmail.com>
2024-01-06 02:30:06 -0500

    adding content section

--- src/app.rs
+++ src/app.rs
@@ -15,11 +15,11 @@ pub fn App() -> impl IntoView {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/leptos-axum-tailwind.css"/>
+        <Body class="bg-gray-900"/>
 
         // sets the document title
         <Title text="Welcome to Leptos"/>
-        // <NavBar/>
-        // <Sidebar/>
+
         // content for this welcome page
         <Router fallback=|| {
             let mut outside_errors = Errors::default();
@@ -30,9 +30,13 @@ pub fn App() -> impl IntoView {
             .into_view()
         }>
             <main>
-                <Routes>
-                    <Route path="/todo" view=HomePage/>
-                </Routes>
+                <NavBar/>
+                <Content/>
+                    
+                
+                    <Routes>
+                        <Route path="/todo" view=HomePage/>
+                    </Routes>
             </main>
         </Router>
         </>
@@ -57,10 +61,23 @@ pub async fn fetch_todo() -> Result<Todo, ServerFnError> {
 }
 
 #[component]
-fn Sidebar() -> impl IntoView {
+fn Content() -> impl IntoView {
     view! {
         <div class="h-screen flex">
-            <aside class="bg-gray-800 w-64 p-4 text-white">
+            <Sidebar/>
+            <main class="w-3/4">
+                <h2 class="text-2xl font-semibold mb-4 text-white">"Main Content"</h2>
+            </main>
+
+        </div>
+
+    }
+}
+
+#[component]
+fn Sidebar() -> impl IntoView {
+    view! {
+            <aside class="w-1/4 bg-gray-800 p-4 text-white">
                 <ul>
                   <li class="mb-4">
                     <a href="#" class="text-gray-400 hover:text-white">Dashboard</a>
@@ -73,6 +90,36 @@ fn Sidebar() -> impl IntoView {
                   </li>
                 </ul>
             </aside>
+    }
+}
+
+#[component]
+fn LoginForm() -> impl IntoView {
+    view! {
+        <div class="w-full max-w-xs">
+            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        "Username"
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+                </div>
+                //     <div class="mb-6">
+                //         <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                //         "Password"
+                //         </label>
+                //         <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+                //         <p class="text-red-500 text-xs italic">Please choose a password.</p>
+                //     </div>
+                //     <div class="flex items-center justify-between">
+                //         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                //         "Sign In"
+                //         </button>
+                //         <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                //         "Forgot Password?"
+                //         </a>
+                //     </div>
+            </form>       
         </div>
     }
 }
@@ -105,28 +152,27 @@ fn HomePage() -> impl IntoView {
 
     view! {
         <div>
-        <Transition fallback=move || view! {<p>"Loading..."</p> }>
-        {
-            
-            move || {
-                todo.get().map(|td| {
-                    match td {
-                        Ok(t) => { 
-                            view! {
-                                <h1>"TEST"</h1>
-                                <p>{t.title}</p>
-                            }.into_view()
-                        },
-                        Err(e) =>{ 
-                            view! { 
-                                <p>{e.to_string()}</p> 
-                            }
-                        }.into_view(),
-                    }
-                }).collect_view()
+            <Transition fallback=move || view! {<p>"Loading..."</p> }>
+            {
+                move || {
+                    todo.get().map(|td| {
+                        match td {
+                            Ok(t) => { 
+                                view! {
+                                    <h1 class="text-white">"TEST"</h1>
+                                    <p>{t.title}</p>
+                                }.into_view()
+                            },
+                            Err(e) =>{ 
+                                view! { 
+                                    <p>{e.to_string()}</p> 
+                                }
+                            }.into_view(),
+                        }
+                    }).collect_view()
+                }
             }
-        }
-        </Transition>
+            </Transition>
         </div>
     }
 }
